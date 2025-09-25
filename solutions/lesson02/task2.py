472b2f52eed916668a43a4ac2abe6eb1a8dc0c6e
Provoda78 <nikbel.2007@mail.ru>
2025-09-25 19:27:58 +0300

    Update task2.py

--- solutions/lesson02/task2.py
+++ solutions/lesson02/task2.py
@@ -1,4 +1,8 @@
 def get_doubled_factorial(num: int) -> int:
     factorial = 1
-    # ваш код
+    if num <= 1:
+        print(1)
+    elif num > 1:
+        for i in range(1, num-1):
+            factorial *= i
     return factorial
