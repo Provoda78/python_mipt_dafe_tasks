c00a43844bd94f150e132cb34971f1df87b14d99
Provoda78 <nikbel.2007@mail.ru>
2025-09-26 11:08:05 +0300

    Update task5.py

--- solutions/lesson02/task5.py
+++ solutions/lesson02/task5.py
@@ -1,3 +1,12 @@
 def get_gcd(num1: int, num2: int) -> int:
-    # ваш код
+    def get_gcd(num1: int, num2: int) -> int:
+    gcd, n  = 1, 0
+    if num2 > num1:
+        n = num1
+        num1 = num2
+        num2 = n
+    while gcd > 0:
+        gcd = num1 % num2
+        num1 = num2
+        num2 = gcd
     return num1
