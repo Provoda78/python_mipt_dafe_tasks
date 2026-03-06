e923f00750d496e920e4c5dc5f72e0e9e5da99c6
Provoda78 <nikbel.2007@mail.ru>
2026-03-06 22:39:31 +0300

    Task03

--- solutions/sem02/lesson03/task1.py
+++ solutions/sem02/lesson03/task1.py
@@ -8,13 +8,21 @@ class ShapeMismatchError(Exception):
 def sum_arrays_vectorized(
     lhs: np.ndarray,
     rhs: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    if lhs.size != rhs.size:
+        raise ShapeMismatchError
+    return lhs + rhs
 
 
-def compute_poly_vectorized(abscissa: np.ndarray) -> np.ndarray: ...
+def compute_poly_vectorized(abscissa: np.ndarray) -> np.ndarray:
+    return ((abscissa**2)*3 + abscissa*2) + 1
 
 
 def get_mutual_l2_distances_vectorized(
     lhs: np.ndarray,
     rhs: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    if len(lhs) != len(rhs):
+        raise ShapeMismatchError
+
+    return (np.sqrt(np.sum((lhs[:, np.newaxis, :] - rhs) ** 2, axis=2))).tolist()
--- solutions/sem02/lesson03/task2.py
+++ solutions/sem02/lesson03/task2.py
@@ -9,11 +9,35 @@ def convert_from_sphere(
     distances: np.ndarray,
     azimuth: np.ndarray,
     inclination: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    if not (distances.shape == azimuth.shape == inclination.shape):
+        raise ShapeMismatchError
+    
+    return (distances * np.sin(inclination)* np.cos(azimuth), 
+            distances * np.sin(inclination) * np.sin(azimuth),
+            distances * np.cos(inclination))
 
 
 def convert_to_sphere(
     abscissa: np.ndarray,
     ordinates: np.ndarray,
     applicates: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    if not (abscissa.shape == ordinates.shape == applicates.shape):
+        raise ShapeMismatchError
+    
+    r = np.sqrt(abscissa**2 + ordinates**2 + applicates**2)
+    if np.all(r == 0):
+        return False
+    if r[r!=0].shape != r.shape:
+        mask = r.copy()
+        mask[mask!=0] = 1
+        mask[mask== 0] = 0
+        r[r==0] = 1
+        applicates *= mask   
+    phi = np.arctan2(ordinates, abscissa)
+    teta = np.arccos(applicates / r)
+    
+    return(r, phi, teta)
+
+print(convert_to_sphere(np.zeros(2), np.zeros(2), np.zeros(2)))
