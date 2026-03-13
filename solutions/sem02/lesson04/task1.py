726a5932684fa6cde466aa4f39baa7fde959e22c
Provoda78 <nikbel.2007@mail.ru>
2026-03-13 23:37:39 +0300

    Lesson04/Task2

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -4,21 +4,21 @@ import numpy as np
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
     if pad_size < 1:
         raise ValueError
-    
+
     if image.ndim == 2:
         height, width = image.shape
         pad_height, pad_width = height + pad_size * 2, pad_size * 2 + width
-        
+
         pad = np.zeros(shape=(pad_height, pad_width), dtype=image.dtype)
-        pad[pad_size:pad_size + height, pad_size:pad_size + width] = image
+        pad[pad_size : pad_size + height, pad_size : pad_size + width] = image
         return pad
-    
+
     if image.ndim == 3:
         height, width, channels = image.shape
         pad_height, pad_width = height + pad_size * 2, pad_size * 2 + width
-        
+
         pad = np.zeros(shape=(pad_height, pad_width, channels), dtype=image.dtype)
-        pad[pad_size:pad_size + height, pad_size:pad_size + width, :] = image
+        pad[pad_size : pad_size + height, pad_size : pad_size + width, :] = image
         return pad
 
 
@@ -28,21 +28,21 @@ def blur_image(
 ) -> np.ndarray:
     if kernel_size % 2 == 0 or kernel_size < 1:
         raise ValueError
-    
+
     padd_image = pad_image(image, kernel_size // 2)
-    
+
     res = np.zeros(image.shape, dtype=image.dtype)
     height, width = image.shape[:2]
-    
+
     for i in range(height):
         for j in range(width):
             if image.ndim == 2:
-                kernel = padd_image[i: i + kernel_size, j: j + kernel_size]
+                kernel = padd_image[i : i + kernel_size, j : j + kernel_size]
                 res[i, j] = np.mean(kernel)
             else:
-                kernel = padd_image[i: i + kernel_size, j: j + kernel_size, :]
+                kernel = padd_image[i : i + kernel_size, j : j + kernel_size, :]
                 res[i, j, :] = np.mean(kernel, axis=(0, 1))
-    
+
     return res.astype(image.dtype)
 
 
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,9 +5,25 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
+
     if threshold < 1:
         raise ValueError
-    
-    
 
-    return 0, 0
+    max_count = -1
+    often_color = 0
+    image_int = image.astype(int)
+
+    for color in range(256):
+        check_mask = image == color
+        if not np.any(check_mask):
+            continue
+
+        count_color = np.sum((np.abs(image_int - color)) < threshold)
+
+        if count_color > max_count:
+            max_count = count_color
+            often_color = color
+
+    procent = max_count / image.size
+
+    return (np.uint8(often_color), float(procent))
