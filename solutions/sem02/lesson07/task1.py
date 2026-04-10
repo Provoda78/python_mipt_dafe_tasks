fabdd777d9e137cb72da04169023c26e54c7863c
Provoda78 <nikbel.2007@mail.ru>
2026-04-10 20:04:27 +0300

    Tasks07

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -1,5 +1,8 @@
 from typing import Any
 
+import matplotlib
+
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -13,8 +16,87 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+
+    if diagram_type not in {"box", "violin", "hist"}:
+        raise ValueError
+    if abscissa.shape != ordinates.shape:
+        raise ShapeMismatchError
+
+    space = 0.2
+    figure = plt.figure(figsize=(8, 8))
+    grid = plt.GridSpec(4, 4, wspace=space, hspace=space)
+
+    axis_scatter = figure.add_subplot(grid[:-1, 1:])
+
+    axis_vert = figure.add_subplot(
+        grid[:-1, 0],
+        sharey=axis_scatter,
+    )
+
+    axis_hor = figure.add_subplot(
+        grid[-1, 1:],
+        sharex=axis_scatter,
+    )
+
+    axis_scatter.scatter(abscissa, ordinates, color="darkred", alpha=0.5)
+
+    if diagram_type == "hist":
+        axis_hor.hist(
+            abscissa,
+            bins=100,
+            color="orange",
+            density=True,
+            alpha=0.5,
+        )
+        axis_vert.hist(
+            ordinates,
+            orientation="horizontal",
+            bins=100,
+            color="red",
+            density=True,
+            alpha=0.5,
+        )
+
+        axis_hor.invert_yaxis()
+        axis_vert.invert_xaxis()
+
+    elif diagram_type == "box":
+        axis_hor.boxplot(
+            abscissa,
+            vert=False,
+            patch_artist=True,
+            boxprops=dict(facecolor="yellow", color="orange"),
+            medianprops=dict(color="red"),
+        )
+        axis_vert.boxplot(
+            ordinates,
+            patch_artist=True,
+            boxprops=dict(facecolor="yellow", color="orange"),
+            medianprops=dict(color="red"),
+        )
+
+        axis_hor.invert_yaxis()
+        axis_vert.invert_xaxis()
+
+    else:
+        v_hor = axis_hor.violinplot(abscissa, vert=False)
+        v_vert = axis_vert.violinplot(ordinates, vert=True)
+
+        for v in [v_hor, v_vert]:
+            for body in v["bodies"]:
+                body.set_facecolor("red")
+                body.set_edgecolor("darkred")
+                body.set_alpha(0.5)
+
+        for v in [v_hor, v_vert]:
+            for part in v:
+                if part == "bodies":
+                    continue
+
+                v[part].set_edgecolor("red")
+
+    axis_hor.invert_yaxis()
+    axis_vert.invert_xaxis()
 
 
 if __name__ == "__main__":
@@ -24,5 +106,8 @@ if __name__ == "__main__":
 
     abscissa, ordinates = np.random.multivariate_normal(mean, cov, size=1000).T
 
+    plt.style.use("ggplot")
     visualize_diagrams(abscissa, ordinates, "hist")
-    plt.show()
+
+    plt.savefig("plot.png")
+    #plt.show()
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,54 @@
-# ваш код (используйте функции или классы для решения данной задачи)
+import matplotlib
+
+matplotlib.use("Agg")
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_info(file: str):
+    with open(file, "r") as f:
+        info = json.load(f)
+
+    before = np.array(info["before"])
+    after = np.array(info["after"])
+    return before, after
+
+
+classes = ["I", "II", "III", "IV"]
+
+befor, after = get_info("data/medic_data.json")
+
+sum_befor, sum_after = {}, {}
+for class_ in classes:
+    sum_befor[class_] = np.sum(befor == class_)
+    sum_after[class_] = np.sum(after == class_)
+
+
+figure, axis = plt.subplots(figsize=(16, 9))
+axis: plt.Axes
+
+plt.style.use("ggplot")
+
+x = np.arange(len(classes))
+width = 0.4
+
+bar1 = axis.bar(
+    x - width / 2, sum_befor.values(), width, label="Before", color="cadetblue", edgecolor="grey"
+)
+
+bar2 = axis.bar(
+    x + width / 2, sum_after.values(), width, label="After", color="lightcoral", edgecolor="grey"
+)
+
+axis.set_title("Mitral disease stages", fontsize=12, fontweight="bold", color="grey")
+axis.set_ylabel("amount of people", fontsize=10, fontweight="bold", color="grey")
+
+axis.set_xticks(x)
+axis.set_xticklabels(classes, fontweight="bold")
+
+
+axis.legend()
+plt.savefig("medic_info.png")
+# plt.show()
